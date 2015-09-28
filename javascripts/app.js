b4b538c3dce51e8ff803886c2b4f2d656957ccbe
Christopher Danan <chrisddan@gmail.com>
2015-09-28 00:24:29 -0700

    Add a random choice generator for the computer's choice. -A random number generator for numbers 0-4 pick a random choice for the computer.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,3 +1,21 @@
+var playerChoice,		//Player's choice for the round.
+	computerChoice;		//Computer's random choice for the round.
+
+/*************
+ * Purpose: Select a random choice of rock, paper, scissors, lizard, or spock for the computer.
+ * Input:
+ 			-none
+ * Output:
+ 			-Return a random choice for the computer.
+*************/
+var randomChoice = function(){
+	//Used to randomly select a choice for the computer.
+	var choices = ["rock", "paper", "scissors", "lizard", "spock"];
+
+	//Randomly choose a choice for the computer.
+	computerChoice = choices[Math.floor(Math.random() * 5)];
+};
+
 var main = function(){
 	"use strict";
 
@@ -10,32 +28,39 @@ var main = function(){
 		$lizard = $("#lizard-choice"),
 		$spock = $("#spock-choice");
 
-	//Player's choice.
-	var playerChoice;
-
 	$rock.on("click", function(){
 		console.log("Clicked rock");
 		playerChoice = "rock";
+
+		randomChoice();
 	});
 
 	$paper.on("click", function(){
 		console.log("Clicked paper");
 		playerChoice = "paper";
+
+		randomChoice();
 	});
 
 	$scissors.on("click", function(){
 		console.log("Clicked scissors");
 		playerChoice = "scissors";
+
+		randomChoice();
 	});
 
 	$lizard.on("click", function(){
 		console.log("Clicked lizard");
 		playerChoice = "lizard";
+
+		randomChoice();
 	});
 
 	$spock.on("click", function(){
 		console.log("Clicked spock");
 		playerChoice = "spock";
+
+		randomChoice();
 	});
 };
 
